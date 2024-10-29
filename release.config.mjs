fa6074cffe5b713fe5bbd3045382d109346ca19c
Danilo Pianini <danilo.pianini@gmail.com>
2024-10-29 11:37:39 +0100

    ci(release): fix the broken configuration of semantic-release

---
+++ release.config.mjs
@@ -0,0 +1,8 @@
+import config from 'semantic-release-preconfigured-conventional-commits' with { type: "json" };
+
+config.plugins.push(
+    "@semantic-release/github",
+    "@semantic-release/git",
+)
+
+export default config;
